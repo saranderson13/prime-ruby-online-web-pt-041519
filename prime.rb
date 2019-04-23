997fa353e9ec0bf9c9214e4cc6b0bad6f9b7c4a5
Sarah Anderson <saranderson13@gmail.com>
2019-04-23 02:32:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  if num <= 1
+    false
+  elsif num == 2 || num == 3
+    true
+  else
+    check = (2..(num - 1))
+    check.to_a.each do |divide_by|
+      return false if num % divide_by == 0
+    end
+    true
+  end
+end
